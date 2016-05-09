b9852028d570da636b8751033fbd78f17963a3c1
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-09 19:42:02 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -218,7 +218,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {85831686}
   }
   save_config()
 end
